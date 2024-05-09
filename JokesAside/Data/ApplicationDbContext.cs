ff45d04653735f54c9096196f89da3532901adff
Myat Thiri Maung <maungmyatthiri@gmail.com>
2024-05-10 03:01:18 +0700

    Add project files.

---
+++ JokesAside/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesAside.Models;
+
+namespace JokesAside.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesAside.Models.Joke> Joke { get; set; } = default!;
+    }
+}
